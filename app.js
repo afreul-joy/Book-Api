507c17f2a53d7c77bc7be1a9c21bb0ab93a03636
Afreul kaium Joy <afreulxoy@gmail.com>
2021-09-02 15:32:12 +0600

    fifth

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const btn = () => {
   const inputFiled = document.getElementById('input-Filed')
   const inputFiledValue = inputFiled.value
   inputFiled.value = ""
-  /// Error Handling
-  if (inputFiledValue === "") {
-    let validation = document.getElementById('valid')
-    validation.innerText =  'No Result Found !!!.'
-  }
+  console.log(inputFiledValue);
   // fetch 
-  const url = `http://openlibrary.org/search.json?q=${inputFiledValue}`
+  const url = `https://openlibrary.org/search.json?q=${inputFiledValue}`
   // adding spinner 
   const spinner = document.getElementById('spinner')
   spinner.classList.remove("d-none")
@@ -23,18 +19,28 @@ const btn = () => {
 }
 
 const gettingResult = (data) => {
-  // Error Handling
-  console.log(data);
-  if (data.length === 0) {
-    let validation = document.getElementById('valid')
-    validation.innerText =  'No Result Found !!!'
-  }
   // Book Search Result 
   document.getElementById('search-result').innerText = data.length  
   const book = document.getElementById('book')
+  const validation = document.getElementById('valid')
+  validation.textContent = "" 
   book.textContent = ""
+  // Error Handling
+  if (data.length === 0) {
+  let errorHandle = document.createElement('div')
+    errorHandle.innerHTML =
+      `
+       <h1>Result Not Found</h1>
+       <p>Try Again</p>
+      `
+   validation.appendChild(errorHandle)
+ 
+  }
+
   // using for Each loop 
-  data.forEach(element => {
+  else {
+    data.forEach(element => {
+    console.log(element);
     // create new element
   const newBook = document.createElement('div')
   newBook.classList.add('col') 
@@ -43,8 +49,9 @@ const gettingResult = (data) => {
        <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="img-fluid " alt="..."></img>
       <div class="card-body">
         <h5 class="card-title">Book Name : ${element.title} </h5>
-        <h5>Author Name : ${element.author_name} </h5>
+        <h6>Author Name : ${element.author_name} </h6>
         <h6>First Published in : ${element.publish_year} </h6>
+        <h6>Publisher : ${element.publisher} </h6>
       </div>
   
     `
@@ -52,4 +59,4 @@ const gettingResult = (data) => {
   });
  
 }
- 
+}
